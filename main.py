19815eb89e94af43f78285bf02d4dab8bbf73427
matsunaga <ygmmkn0717@gmail.com>
2022-11-24 06:21:00 +0900

    [clean] 送信メッセージのリファクタリング
    多様する文字列をconfigファイルから読み込み

--- main.py
+++ main.py
@@ -28,79 +28,77 @@ def add_embed(title, descrip, color):
 
 @client.event
 async def on_raw_reaction_add(payload): #ロール付与機能
-    if payload.message_id == config_ini.getint('MESSAGE', 'splFes_msg_id_2'): 
+    guild_id = config_ini.getint('GUILD', 'guild_id')
+    if payload.message_id == config_ini.getint('MESSAGE_ID', 'splFes_msg_id_2'): 
         checked_emoji = payload.emoji.id 
-        guild_id = payload.guild_id 
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
         #channel_id = config_ini.getint('CHANNEL', 'splFes_ch_id')
         #channel = client.get_channel(channel_id)
         member = guild.get_member(payload.user_id)
-        role_bool = 1
-        if checked_emoji == config_ini.getint('FUKA', 'emoji_id'):
-            await payload.member.add_roles(guild.get_role(config_ini.getint('FUKA', 'role_id')))
-            msg = 'フウカのロールを付与しました！'
-        elif checked_emoji == config_ini.getint('UTSUHO', 'emoji_id'):
-            await payload.member.add_roles(guild.get_role(config_ini.getint('UTSUHO', 'role_id')))
-            msg = 'ウツホのロールを付与しました！'
-        elif checked_emoji == config_ini.getint('MANTARO', 'emoji_id'): 
-            await payload.member.add_roles(guild.get_role(config_ini.getint('MANTARO', 'role_id')))
-            msg = 'マンタローのロールを付与しました！'
-    elif payload.message_id == config_ini.getint('MESSAGE', 'pokemonSV_msg_id'): 
+        change_reaction = 1
+        if checked_emoji == config_ini.getint('EMOJI_ID', 'fuka'):
+            await payload.member.add_roles(guild.get_role(config_ini.getint('ROLE_ID', 'fuka')))
+            msg = config_ini.get('TEXT', 'fuka') + 'のロールを付与しました！'
+        elif checked_emoji == config_ini.getint('EMOJI_ID', 'utsuho'):
+            await payload.member.add_roles(guild.get_role(config_ini.getint('ROLE_ID', 'utsuho')))
+            msg = config_ini.get('TEXT', 'utsuho') + 'のロールを付与しました！'
+        elif checked_emoji == config_ini.getint('EMOJI_ID', 'mantaro'): 
+            await payload.member.add_roles(guild.get_role(config_ini.getint('ROLE_ID', 'mantaro')))
+            msg = config_ini.get('TEXT', 'mantaro') + 'のロールを付与しました！'
+    elif payload.message_id == config_ini.getint('MESSAGE_ID', 'pokemonSV_msg_id'): 
         checked_emoji = payload.emoji.id 
-        guild_id = payload.guild_id 
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
         member = guild.get_member(payload.user_id)
-        role_bool = 1
-        if checked_emoji == config_ini.getint('KORAIDON', 'emoji_id'): 
-            await payload.member.add_roles(guild.get_role(config_ini.getint('KORAIDON', 'role_id')))
-            msg = 'スカーレットのロールを付与しました！'
-        elif checked_emoji == config_ini.getint('MIRAIDON', 'emoji_id') : 
-            await payload.member.add_roles(guild.get_role(config_ini.getint('MIRAIDON', 'role_id')))
-            msg = 'バイオレットのロールを付与しました！'
-    elif payload.message_id == config_ini.getint('MESSAGE', 'test_msg_id'): 
+        change_reaction = 1
+        if checked_emoji == config_ini.getint('EMOJI_ID', 'scarlet'): 
+            await payload.member.add_roles(guild.get_role(config_ini.getint('ROLE_ID', 'scarlet')))
+            msg = config_ini.get('TEXT', 'scarlet') + 'のロールを付与しました！'
+        elif checked_emoji == config_ini.getint('EMOJI_ID', 'violet') : 
+            await payload.member.add_roles(guild.get_role(config_ini.getint('ROLE_ID', 'violet')))
+            msg = config_ini.get('TEXT', 'violet') + 'のロールを付与しました！'
+    elif payload.message_id == config_ini.getint('MESSAGE_ID', 'test_msg_id'): 
         print(payload.emoji.id)
         print_time_line()
-        role_bool = 0
+        change_reaction = 0
     else :
-        role_bool = 0
-    if role_bool == 1:
-        embed = add_embed('ロール変更', msg, int(config_ini.get('COLOR', 'role_lightBlue'),16))
+        change_reaction = 0
+    if change_reaction == 1:
+        embed = add_embed('ロール更新', msg, int(config_ini.get('COLOR', 'role_lightBlue'),16))
         await payload.member.send(embed=embed)
         print_info(member,msg)
 
 @client.event
 async def on_raw_reaction_remove(payload):
-    if payload.message_id == config_ini.getint('MESSAGE', 'splFes_msg_id_2'):
+    guild_id = config_ini.getint('GUILD', 'guild_id')
+    if payload.message_id == config_ini.getint('MESSAGE_ID', 'splFes_msg_id_2'):
         checked_emoji = payload.emoji.id
-        guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
         member = guild.get_member(payload.user_id)
-        role_bool = 1
-        if checked_emoji == config_ini.getint('FUKA', 'emoji_id'):
-            await member.remove_roles(guild.get_role(config_ini.getint('FUKA', 'role_id')))
-            msg = 'フウカのロールを外しました！'
-        if checked_emoji == config_ini.getint('UTSUHO', 'emoji_id'): 
-            await member.remove_roles(guild.get_role(config_ini.getint('UTSUHO', 'role_id')))
-            msg = 'ウツホのロールを外しました！'
-        if checked_emoji == config_ini.getint('MANTARO', 'emoji_id'): 
-            await member.remove_roles(guild.get_role(config_ini.getint('MANTARO', 'role_id')))
-            msg = 'マンタローのロールを外しました！'
-    elif payload.message_id == config_ini.getint('MESSAGE', 'pokemonSV_msg_id'): 
+        change_reaction = 1
+        if checked_emoji == config_ini.getint('EMOJI_ID', 'fuka'):
+            await member.remove_roles(guild.get_role(config_ini.getint('ROLE_ID', 'fuka')))
+            msg = config_ini.get('TEXT', 'fuka') + 'のロールを外しました！'
+        if checked_emoji == config_ini.getint('EMOJI_ID', 'utsuho'): 
+            await member.remove_roles(guild.get_role(config_ini.getint('ROLE_ID', 'utsuho')))
+            msg = config_ini.get('TEXT', 'utsuho') + 'のロールを外しました！'
+        if checked_emoji == config_ini.getint('EMOJI_ID', 'mantaro'): 
+            await member.remove_roles(guild.get_role(config_ini.getint('ROLE_ID', 'mantaro')))
+            msg = config_ini.get('TEXT', 'mantaro') + 'のロールを外しました！'
+    elif payload.message_id == config_ini.getint('MESSAGE_ID', 'pokemonSV_msg_id'): 
         checked_emoji = payload.emoji.id
-        guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
         member = guild.get_member(payload.user_id)
-        role_bool = 1
-        if checked_emoji == config_ini.getint('KORAIDON', 'emoji_id'):
-            await member.remove_roles(guild.get_role(config_ini.getint('KORAIDON', 'role_id')))
-            msg = 'スカーレットのロールを外しました！'
-        if checked_emoji == config_ini.getint('MIRAIDON', 'emoji_id'):
-            await member.remove_roles(guild.get_role(config_ini.getint('MIRAIDON', 'role_id')))
-            msg = 'バイオレットのロールを外しました！'
+        change_reaction = 1
+        if checked_emoji == config_ini.getint('EMOJI_ID', 'scarlet'):
+            await member.remove_roles(guild.get_role(config_ini.getint('ROLE_ID', 'scarlet')))
+            msg = config_ini.get('TEXT', 'scarlet') + 'のロールを外しました！'
+        if checked_emoji == config_ini.getint('EMOJI_ID', 'violet'):
+            await member.remove_roles(guild.get_role(config_ini.getint('ROLE_ID', 'violet')))
+            msg = config_ini.get('TEXT', 'violet') + 'のロールを外しました！'
     else :
-        role_bool = 0
-    if role_bool == 1:
-        embed = add_embed('ロール変更', msg, int(config_ini.get('COLOR', 'role_lightBlue'),16))
+        change_reaction = 0
+    if change_reaction == 1:
+        embed = add_embed('ロール更新', msg, int(config_ini.get('COLOR', 'role_lightBlue'),16))
         await member.send(embed=embed)
         print_info(member,msg)
 
